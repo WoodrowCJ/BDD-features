1ecbc8b44aa832416faa7c1d13a9087ca11f8189
Carl Woodrow <dropshipalbum@gmail.com>
2021-09-29 14:48:33 +0100

    how old are you conditionals added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,128 +1,33 @@
-/*
-let myObject;
-console.log(myObject);
-myObject = {
-    key: `value`,
-    age : `50ish`,
-    realAge : 52
-};
 
-console.log(myObject);
+// CONDITIONALS
 
-// interpolation
+// let a = 2;
 
-console.log(`2 + 2 = ${2+2}`);
+// if ( a == 0 ) {
+//     console.log(`success!`);
+// }
+// else if ( a != 2 ) {
+//     console.log(`not 2`);
+// } else {
+//     console.log(`it must be 2`);
+// }
+let b = 1;
+// b ? console.log(`true`) : console.log(`false`);
 
-let totalmoney = 4000;
-let moneyPaidSoFar = 2348;
-let totalLeftToPay;
+b === 1 ? console.log(`Yes`) : console.log(`No`);
 
-console.log(`The total bill is £${totalmoney} the remaining amount of money to be paid is £${totalmoney-moneyPaidSoFar}`);
-console.log(`Please feel free to leave a Tip!`);
-*/
+//Exercise 1
 
-//Iterations
-/* FOR loops
-Declare a variable
-set condition
-declaring step
+let age = 70;
 
+//Exercise 2
 
-for(let i = 0; i < 10; i++ ) {
-   // console.log(`i = ${i}`);
-};
-
-let condition = true;
-let increment = 0;
-
-while(condition) {
-    if(increment == 10) {
-        condition = false;
-        break;
-    }
-   // console.log(`increment = ${increment}`);
-    increment++;
-};
-
-// DO-WHILE  loop
-// the same as while, but always runs at least once
-// SWITCH cases
-
-let number = 1;
-
-switch(number) {
-    case 0:
-        console.log(`the number is 0`);
-        break;
-    case 1:
-        console.log(`the number is 1`);
-        break;
-    default:
-        console.warn(`what the deuce!`);
+if (age >= 18 && age <= 65) {
+    console.log(`within Age range at ` + age);
+} else if(age < 18) {
+    console.log(`sorry you are underage being ` + age);
+} else {
+    console.log(`Have you considered retirement? ` + age);
 }
 
-*/
-// Exercise one
-
-for(let a = 100; a < 201; a++) {
-    console.log(`A = ${a}`);
-};
-
-// Exercise two
-
-a = 100;
-//console.log(a);
-while(a<=200) {
-    if(a%2 == 0) {
-        console.log(`-` + a);
-    }
-        else {
-        console.log(`*` + a);
-    }
-    a++;
-}
-console.log(`The End!`);
-
-//exercise three
-
- let condition = true;
- let increment = 1;
- 
-
-while(condition) {
-    if(increment == 11) {
-        condition = false;
-        break;
-    }
-   console.log(`increment = ${increment}`);
-
-   for(let i = 1; i < 11; i++ ) {
-    console.log(`i = ${i}`);
- };
-
-
-    increment++;
-};
-
-//exercise four
-
-let dayOFWeek = 'Wednesday';
-
-switch(dayOFWeek) {
-    case 'Monday':
-        console.log(`Mon`);
-    case 'Tuesday':
-        console.log(`Tue`);
-    case 'Wednesday':
-        console.log(`Wed`);
-    case 'Thursday':
-        console.log(`Thu`);
-    case 'Friday':
-        console.log(`Fri`);
-    case 'Saturday':
-        console.log(`It's the weekend!`);
-        break;
-    
-    default:
-        console.warn(`what the deuce!`);
-}
+age > 50 ? console.log(`Yes`) : console.log(`No`);
