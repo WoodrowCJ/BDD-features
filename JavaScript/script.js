7045afa0247383d5d92f122e25a473c1368125b9
Carl Woodrow <dropshipalbum@gmail.com>
2021-09-29 15:21:54 +0100

    added myArray messages and iterations

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -11,23 +11,72 @@
 // } else {
 //     console.log(`it must be 2`);
 // }
-let b = 1;
-// b ? console.log(`true`) : console.log(`false`);
+// let b = 1;
+// // b ? console.log(`true`) : console.log(`false`);
 
-b === 1 ? console.log(`Yes`) : console.log(`No`);
+// b === 1 ? console.log(`Yes`) : console.log(`No`);
 
-//Exercise 1
+// //Exercise 1
 
-let age = 70;
+// let age = 70;
 
-//Exercise 2
+// //Exercise 2
 
-if (age >= 18 && age <= 65) {
-    console.log(`within Age range at ` + age);
-} else if(age < 18) {
-    console.log(`sorry you are underage being ` + age);
-} else {
-    console.log(`Have you considered retirement? ` + age);
+// if (age >= 18 && age <= 65) {
+//     console.log(`within Age range at ` + age);
+// } else if(age < 18) {
+//     console.log(`sorry you are underage being ` + age);
+// } else {
+//     console.log(`Have you considered retirement? ` + age);
+// }
+
+// age > 50 ? console.log(`Yes`) : console.log(`No`);
+
+// OBJECTS
+
+// let vehicle = {
+//     "type": "car",
+//     "seats": 5,
+//     "wheels": 4
+// };
+// console.log(vehicle);
+
+// let house = [
+//     vehicle,
+//     {"type": "bike", "seats": 1, "wheels": 2}
+// ];
+
+// console.log(house);
+
+// let x = [1,2,3,4,5];
+// let y = [12,-9,12,1,45];
+// for (let i = 0; i < x.length; i++) {
+// //    console.log(x[i]);
+// }
+
+// for (let i of y) {
+//     console.log(i);
+// }
+
+// JSON NOTATION
+
+// let newJSON = {
+//     "name": "Carl",
+//     "age" : 50
+// }
+
+// let myName = JSON.parse(`{"name" : "Fred"}`);
+// console.log(myName.name);
+
+// let str = JSON.stringify(newJSON);
+// console.log(str);
+
+let myArray = ["hello","everyone"];
+console.log(myArray.length);
+myArray.push("hope","it's","going","great");
+console.log(myArray.length);
+myArray.shift(-1);
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
 }
-
-age > 50 ? console.log(`Yes`) : console.log(`No`);
+ 
